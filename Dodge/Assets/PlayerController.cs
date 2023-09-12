b5950c0fcd902f354023314581e8fdbd39dac9f1
Frenmon <141483780+Frenmon@users.noreply.github.com>
2023-09-13 08:39:23 +0900

    260
    260

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -4,43 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;
+    private Rigidbody playerRigidbody;
     public float speed = 8f;
         
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>()
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true )
+        if (Input.GetKey(KeyCode.UpArrow) == true)
         {
             playerRigidbody.AddForce(0f, 0f, speed);
-        
-        
+
+
         }
-        
-        if (Input.GetKey(KeyCode.DownArrow) == true )
+
+        if (Input.GetKey(KeyCode.DownArrow) == true)
         {
             playerRigidbody.AddForce(0f, 0f, -speed);
-        
-        
+
+
         }
-        
-        if (Input.GetKey(KeyCode.RightArrow) == true )
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
         {
             playerRigidbody.AddForce(speed, 0f, 0f);
-        
-        
+
+
         }
-        
-        if (Input.GetKey(KeyCode.LeftArrow) == true )
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
             playerRigidbody.AddForce(-speed, 0f, 0f);
-        
-        
+
+
         }
+        
+        
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
 }
