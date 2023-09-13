5d1e94ba5766b86e55b9bc58ceb8579139b1b95b
Frenmon <141483780+Frenmon@users.noreply.github.com>
2023-09-13 10:03:10 +0900

    263

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -10,39 +10,20 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerRigidbody = GetComponent<Rigidbody>()
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, speed);
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
 
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
 
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
-
-
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-
-
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-
-
-        }
+        Vector3 newVelocity = new Vector3(xSpeed, 0, zSpeed);
+        playerRigidbody.velocity = newVelocity;
         
         
     }
